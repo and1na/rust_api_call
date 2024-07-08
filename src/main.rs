d211633b0441aed77e40055408085ce9e4bc173d
Dani Andina <145332542+and1na@users.noreply.github.com>
2024-07-08 18:28:17 +0200

    implemented api call to repo stargazers

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,43 @@ struct User{
     id: u32
 }
 
-fn main() {
-    // Your code here
-    println!("hola");
+/*
+Se utiliza para configurar una función como el punto de entrada principal
+de una aplicación asincrónica que utiliza Tokio como el runtime asincrónico.
+ */
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    //URL de la API
+    let url: String = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "rust-lang-nursery",
+        repo = "rust-cookbook");
+
+    println!("URL: {}", url);
+
+
+    let client = reqwest::Client::new();
+
+    //&url es una referencia a la variable url, no es como pasarle directamente la variable
+    //esto es por el moving de Rust (cambia la propiedad de la variable) y luego no puedes
+    //cambiarla de nuevo 
+    let response = client
+        .get(&url)
+        .header(USER_AGENT, "rust web-api-client demo")
+        .send()
+        //el ? es para manejar el error de manera más limpia, si la expresión devuelve un error
+        //se devuelve el error, si no, se sigue con la ejecución (asigna el valor a la variable)
+        .await?;
+
+    let users : Vec<User> = response.json().await?;
+
+    println!("{:?}", users);
+
+
+    //Aqui se podria escribir o "Ok(())" o "return Ok(())"
+    //porque el return implicito de Rust se escribe sin el punto y coma
+    //en el caso de que pusiera el ; solo se evaluaria la funcion pero no
+    //se devolveria el valor
+    return Ok(());
 }
+
